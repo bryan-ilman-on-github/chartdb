117b89a8ee15950dad0dbd1c5c173a53bb9dbb4b
Bryan 'Ilman <165170055+bryan-ilman-on-github@users.noreply.github.com>
2026-02-22 19:31:08 +0800

    fix: simplify envsubst command in entrypoint.sh
    Removed redundant line for envsubst command.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-envsubst '${OPENAI_API_KEY} ${OPENAI_API_ENDPOINT} ${LLM_MODEL_NAME} ${HIDE_CHARTDB_CLOUD} ${DISABLE_ANALYTICS}' \      
-  < /etc/nginx/conf.d/default.conf.template \
-  > /etc/nginx/conf.d/default.conf
-
-# Replace port 80 with $PORT for Render compatibility
+envsubst '${OPENAI_API_KEY} ${OPENAI_API_ENDPOINT} ${LLM_MODEL_NAME} ${HIDE_CHARTDB_CLOUD} ${DISABLE_ANALYTICS}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 sed -i "s/listen       80;/listen       ${PORT};/" /etc/nginx/conf.d/default.conf
 sed -i "s/listen  \[::\]:80;/listen  [::]:${PORT};/" /etc/nginx/conf.d/default.conf
-
 nginx -g "daemon off;"
